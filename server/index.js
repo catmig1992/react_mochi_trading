8ea38822f6fc1e97ebb381b7f07c19c279f0b46b
Catherine Miguel <catmig1992@gmail.com>
2024-01-15 23:59:37 -0800

    update 404 page style

--- server/index.js
+++ server/index.js
@@ -15,10 +15,15 @@ app.get("/", function (req, res) {
 });
 
 app.get("*", (req, res) => {
-  res.status(404).send(`<h1>404 Page</h1>`);
+  res.status(404).send(`
+    <body style="margin:0">
+      <div style="border:1px solid black; height:10vh; background-color:red">
+        <h1 style="text-align:center; color:white">404 Page</h1>
+      </div>
+    </body>
+  `);
 });
 
 app.listen(process.env.PORT, function () {
   console.log("Ready!");
 });
-
