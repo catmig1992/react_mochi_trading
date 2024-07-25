9ae84764b57ba03a3c5473d7d86de27ab15e1b9c
Catherine Miguel <catmig1992@gmail.com>
2024-07-25 04:33:16 -0700

    test model file and show route using an array

--- server/controllers/events_controller.js
+++ server/controllers/events_controller.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const events = express.Router();
+const Event = require("../models/event.js");
 
 // INDEX ROUTE
 events.get("/", (req, res) => {
-  res.send("GET /events stub");
+  res.send(Event);
+});
+
+// CREATE
+events.post("/", (req, res) => {
+  res.send("POST /events stub");
+});
+
+// SHOW
+events.get("/:arrayIndex", (req, res) => {
+  res.send(Event[req.params.arrayIndex]);
 });
 
 module.exports = events;
